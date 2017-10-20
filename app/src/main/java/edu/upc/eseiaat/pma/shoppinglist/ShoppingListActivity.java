6efbaa31cb9db0a852dfd5a5078ddfea70c8ca4f
Jordi Ogños <jordiognosaniceto@gmail.com>
2017-10-20 16:38:10 +0200

    funciona toda la fase 1

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -1,9 +1,12 @@
 package edu.upc.eseiaat.pma.shoppinglist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,7 +39,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         itemList.add("Zanahorias");
         itemList.add("Copas danone");
 
-        adapter=new ArrayAdapter<String>(
+        adapter=new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1,
                 itemList
         );
@@ -51,13 +54,39 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyevent) {
-                addItem();
-                return true;
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+               addItem();
+               return true;
             }
         });
 
         list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                maybeRemoveItem(position);
+                return true;
+
+            }
+        });
+    }
+
+    private void maybeRemoveItem( final int position) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        String fmt =getResources().getString(R.string.confirm_message);
+        builder.setMessage(String.format(fmt,itemList.get(position)));
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                itemList.remove(position);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
     }
 
     private void addItem() {
