2cb7215a14029d404e26576a605bdda1feed3292
Jordi Ogños <jordiognosaniceto@gmail.com>
2017-10-20 15:51:36 +0200

    ShoppingList Part 1, se pueden añadir cosas a la lista y sale bien en la pantalla, tenmos un relative layout dentro del global y un button +

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -0,0 +1,58 @@
+package edu.upc.eseiaat.pma.shoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        list= (ListView) findViewById(R.id.list);
+        btn_add=(Button) findViewById(R.id.btn_add);
+        edit_item=(EditText) findViewById(R.id.editItem);
+
+        itemList= new ArrayList<>();
+        itemList.add("Patatas");
+        itemList.add("Papel de water");
+        itemList.add("Zanahorias");
+        itemList.add("Copas danone");
+
+        adapter=new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, itemList);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
+        list.setAdapter(adapter);
+
+
+
+    }
+
+    private void addItem() {
+        String item_text = edit_item.getText().toString();
+        itemList.add(item_text);
+        adapter.notifyDataSetChanged();
+    }
+}
